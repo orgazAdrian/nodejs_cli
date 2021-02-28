b074fa87dd4a4a28e71efaf035a28dae6b32a89c
Adrian Orgaz <63147655+orgazAdrian@users.noreply.github.com>
2021-02-28 20:42:22 +0100

    Add files via upload

---
+++ create-wallet.js
@@ -0,0 +1,8 @@
+
+const ethers = require('ethers');
+
+const wallet = ethers.Wallet.createRandom();
+
+console.log(`Mnemonic: ${wallet.mnemonic.phrase}`);
+console.log(`Address: ${wallet.address}`);
+
